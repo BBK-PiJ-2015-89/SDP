5d40a4ce9907fc180c4e5ff6322df554144eb64a
Graeme WIlkinson <gwilki01@mail.bbk>
2017-01-12 20:01:17 +0000

    section 6 complete.

--- exercises/week01/src/Driver.java
+++ exercises/week01/src/Driver.java
@@ -2,9 +2,12 @@ public class Driver {
     public static void main(String[] args) {
         Storage<BankAccount> aStorage = new Storage<>();
         Storage<String> sStorage = new Storage<>();
-        Class baCls = BankAccount.class;
+        Class<BankAccount> baCls = BankAccount.class;
+
+
         try {
-            Object myAccount = baCls.newInstance();
+            BankAccount myAccount = (BankAccount) baCls.newInstance();
+
             aStorage.setValue(myAccount);
 // Deposit
             myAccount.deposit(15);
