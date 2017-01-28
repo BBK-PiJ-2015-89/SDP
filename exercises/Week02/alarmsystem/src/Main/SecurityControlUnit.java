53871763efd06f8a4fd6811089648ea69f0ef2dd
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2017-01-28 21:20:04 +0000

    adding time checker

--- exercises/Week02/alarmsystem/src/Main/SecurityControlUnit.java
+++ exercises/Week02/alarmsystem/src/Main/SecurityControlUnit.java
@@ -1,7 +1,25 @@
 package Main;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
 /**
  * Created by graemewilkinson on 28/01/17.
  */
-public class SecurityControlUnit {
+public class SecurityControlUnit extends ControlUnit{
+
+
+    public SecurityControlUnit(List<Sensor> sensorList) {
+        super(sensorList);
+    }
+
+    public boolean timeCheck(){
+        String timeStamp = new SimpleDateFormat("HHmm").format(Calendar.getInstance().getTime());
+        int result = Integer.parseInt(timeStamp);
+        return result > 2159 && result < 600;
+    }
+
+
+
 }
