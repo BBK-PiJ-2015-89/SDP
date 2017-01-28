2b938bc57aaa5996aa6cebba15d99560ba35c0d0
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2017-01-28 18:47:16 +0000

    realised I'm using my own files rather than keiths so have added some.

--- exercises/Week02/alarmsystem/src/Main/ControlUnit.java
+++ exercises/Week02/alarmsystem/src/Main/ControlUnit.java
@@ -1,8 +1,21 @@
 package Main;
 
-/**
- * Created by graemewilkinson on 28/01/17.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class ControlUnit {
 
+  public void pollSensors() {
+    List<SensorInt> sensors = new ArrayList<SensorInt>();
+    sensors.add(new FireSensor("Living Room"));
+    sensors.add(new SmokeSensor("Loft"));
+
+    for (SensorInt sensor : sensors) {
+      if (sensor.isTriggered()) {
+        System.out.println("A " + sensor.getSensortype() + " sensor was triggered at " + sensor.getLocation());
+      } else {
+        System.out.println("Polled " + sensor.getSensortype() + " at " + sensor.getLocation() + " successfully");
+      }
+    }
+  }
 }
