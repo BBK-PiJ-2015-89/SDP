f928fe87d81703ab5e6cbabfad8fc64c679f0e28
Graeme WIlkinson <gwilki01@mail.bbk>
2017-03-12 17:30:43 +0000

    first attempt at object

--- exercises/week05/src/main/scala/PersonPackage/Person.scala
+++ exercises/week05/src/main/scala/PersonPackage/Person.scala
@@ -9,4 +9,13 @@ class Person (var firstName: String, var lastName: String){
 }
 object Person{
 
+  def apply (name: String): Person ={
+    val namessplit = name.split(" ")
+    val newBorn = new Person (firstName = "", lastName = "")
+    newBorn.firstName = namessplit(0)
+    newBorn.lastName = namessplit(1)
+    newBorn
+  }
+
+
 }
