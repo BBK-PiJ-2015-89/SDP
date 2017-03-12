b1e10a9c8465c890a8eb400baa2d7af197896e02
Graeme WIlkinson <gwilki01@mail.bbk>
2017-03-12 17:26:32 +0000

    Constructed the Person with the correct type of class and constructor

--- exercises/week05/src/main/scala/PersonPackage/Person.java
+++
@@ -1,9 +0,0 @@
-package PersonPackage;
-
-/**
- * Created by gwilki01 on 12/03/2017.
- */
-public class Person {
-
-
-}
---
+++ exercises/week05/src/main/scala/PersonPackage/Person.scala
@@ -0,0 +1,12 @@
+package PersonPackage
+
+/**
+  * Created by gwilki01 on 12/03/2017.
+  */
+class Person (var firstName: String, var lastName: String){
+
+
+}
+object Person{
+
+}
