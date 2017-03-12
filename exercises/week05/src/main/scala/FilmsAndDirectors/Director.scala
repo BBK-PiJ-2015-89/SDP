65241845fba50ece138d0d1378842e3bed44d595
Graeme WIlkinson <gwilki01@mail.bbk>
2017-03-12 17:56:13 +0000

    director populated as per spec

---
+++ exercises/week05/src/main/scala/FilmsAndDirectors/Director.scala
@@ -0,0 +1,10 @@
+package FilmsAndDirectors
+
+/**
+  * Created by gwilki01 on 12/03/2017.
+  */
+class Director (firstName : String, lastName: String, yearOfBirth: Int){
+
+  def name = firstName + " " + lastName
+
+}
---
+++ exercises/week05/src/main/scala/FilmsAndDirectors/Film.scala
@@ -0,0 +1,8 @@
+package FilmsAndDirectors
+
+/**
+  * Created by gwilki01 on 12/03/2017.
+  */
+class Film {
+
+}
