a54fd6b5c3e328d5d003a7b9b093c426eca7f9c6
Graeme WIlkinson <gwilki01@mail.bbk>
2017-03-12 16:06:26 +0000

    counter class implemented.

--- exercises/week05/src/main/scala/Counter.scala
+++ exercises/week05/src/main/scala/Counter.scala
@@ -1,8 +1,23 @@
 /**
   * Created by graemewilkinson on 05/03/2017.
   */
-class Counter {
+class Counter (var number : Int) {
 
-  
+  def inc (): Counter = {
+    new Counter(number + 1)
+  }
 
+  def dec (): Counter = {
+    new Counter(number -1)
+  }
+
+  def count () : Int = {
+    number
+  }
+}
+
+object Test extends App {
+  val x = new Counter(10).inc().inc().count()
+
+  println(x)
 }
