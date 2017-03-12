393d591c1940071fa86e73e83096e64914ce700f
Graeme WIlkinson <gwilki01@mail.bbk>
2017-03-12 18:02:24 +0000

    populated film class

--- exercises/week05/src/main/scala/FilmsAndDirectors/Film.scala
+++ exercises/week05/src/main/scala/FilmsAndDirectors/Film.scala
@@ -3,6 +3,14 @@ package FilmsAndDirectors
 /**
   * Created by gwilki01 on 12/03/2017.
   */
-class Film {
+class Film (name: String, yearofRelease: Int, imdbRating: Double, director: Director) {
+
+  def directorsAge : Int = {
+    val age = yearofRelease - directorsAge
+    age
+  }
+
+  def isDirectedBy (directed: Director) : Boolean = director==directed
+
 
 }
